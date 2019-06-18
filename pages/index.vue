126dca4b7c8d7cbebdf99a1bad18ae8ab21565c4
DaviTeodoro <davi.a.teodoro@gmail.com>
2019-06-18 19:27:44 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="container">
+    <div></div>
+    <div class="photo-card" v-for="photo in photos" :key="photo._id">
+      <!-- <div class="photo-item">
+        <p>{{photo._id}}</p>
+        <img class="photo" :src="`/photos/${photo._id}.jpg`" alt>
+      </div>-->
+      <div class="similars">
+        <div
+          v-for="(similar, iSimilar) in photo.neighbors"
+          :key="similar.filename"
+          class="photo-item"
+          @click="selectSimilar(photo, iSimilar, similar.filename )"
+        >
+          <img
+            class="photo"
+            :class="{ active: similar.isSamePhoto }"
+            :src="`/photos/${similar.filename}.jpg`"
+            alt
+          >
+          <p>ID: {{similar.filename}} Similarity: {{similar.similarity * 100}}%</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+import _ from 'lodash'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      _photos: [
+        {
+          _id: '001AAN005147',
+          fileName: '001AAN005147.json',
+          neighbors: [
+            { filename: '001AAN005147', similarity: 1 },
+            { filename: '001AAN005149', similarity: 0.8897 },
+            { filename: '001AN01002020', similarity: 0.8167 },
+            { filename: '002080RJ0103', similarity: 0.8064 },
+            { filename: '0071824cx001-09', similarity: 0.7964 },
+            { filename: '001AN01002021', similarity: 0.7897 },
+            { filename: '002027BRRJ021', similarity: 0.788 },
+            { filename: '007SN-02', similarity: 0.7868 },
+            { filename: '002001MF003001', similarity: 0.7842 },
+            { filename: '002080RJ2202', similarity: 0.78 },
+            { filename: '001AN01002019', similarity: 0.7691 },
+            { filename: '001AN01001005', similarity: 0.7618 },
+            { filename: '007A6P3F03-045', similarity: 0.7589 },
+            { filename: '007ALA089', similarity: 0.7529 },
+            { filename: '007A5P4F05-038-039', similarity: 0.7515 },
+            { filename: '002080BR0215', similarity: 0.7506 },
+            { filename: '001ANV022024', similarity: 0.7461 },
+            { filename: '007A5P3F13-036', similarity: 0.7412 },
+            { filename: 'CJHF1007', similarity: 0.7399 },
+            { filename: '007A5P3F13-035', similarity: 0.735 },
+            { filename: 'CJHF3003', similarity: 0.7348 },
+            { filename: '0071824cx036a-09', similarity: 0.7347 },
+            { filename: '001ALA011008', similarity: 0.7345 },
+            { filename: '001AMF015002', similarity: 0.733 },
+            { filename: '007A5P4F05-070a072', similarity: 0.7317 },
+            { filename: '007A5P4F05-043', similarity: 0.7315 },
+            { filename: '0071824cx045-04', similarity: 0.7267 },
+            { filename: '001ALA011005', similarity: 0.7267 },
+            { filename: '001MF003001', similarity: 0.7181 },
+            { filename: '007A5P3F13-022', similarity: 0.7181 }
+          ]
+        },
+        {
+          _id: '001AAN005149',
+          fileName: '001AAN005149.json',
+          neighbors: [
+            { filename: '001AAN005149', similarity: 1 },
+            { filename: '001AAN005147', similarity: 0.8897 },
+            { filename: '0071824cx036a-09', similarity: 0.8487 },
+            { filename: '0071824cx001-09', similarity: 0.8454 },
+            { filename: '001AN01002019', similarity: 0.8417 },
+            { filename: '001AN01002021', similarity: 0.8312 },
+            { filename: '002080RJ2202', similarity: 0.8267 },
+            { filename: '007SN-02', similarity: 0.8265 },
+            { filename: '001AN01001005', similarity: 0.8251 },
+            { filename: '001AN01002020', similarity: 0.8226 },
+            { filename: '007A6P3F03-045', similarity: 0.8224 },
+            { filename: '007A5P4F05-038-039', similarity: 0.8172 },
+            { filename: '007A5P3F13-036', similarity: 0.8131 },
+            { filename: '007A5P4F05-070a072', similarity: 0.7944 },
+            { filename: '002080RJ0103', similarity: 0.7914 },
+            { filename: '007A5P4F05-043', similarity: 0.788 },
+            { filename: '001MF003001', similarity: 0.7822 },
+            { filename: 'P002SAm52-0054', similarity: 0.7801 },
+            { filename: '0071824cx017a-04', similarity: 0.7741 },
+            { filename: '001ALA011005', similarity: 0.772 },
+            { filename: '007A5P3F13-022', similarity: 0.7687 },
+            { filename: '0071824cx045-04', similarity: 0.7678 },
+            { filename: '007A6P4FP15-010', similarity: 0.7668 },
+            { filename: 'CJHF3003', similarity: 0.7653 },
+            { filename: '002080BR0215', similarity: 0.7643 },
+            { filename: '0071824cx036a-04', similarity: 0.7639 },
+            { filename: '001ANV022024', similarity: 0.7636 },
+            { filename: '0071824cx036a-02', similarity: 0.7606 },
+            { filename: '001MF008', similarity: 0.7601 },
+            { filename: 'CJHF1007', similarity: 0.7588 }
+          ]
+        }
+      ]
+    }
+  },
+  async asyncData({ route }) {
+    console.log('parametros são:', route.query)
+    const { similarity } = route.query
+    const { data } = await axios.get(
+      `http://localhost:3000/api/?similarity=${similarity}`
+    )
+    return { photos: data }
+  },
+  methods: {
+    //takes the photo, index and id of the similar photo and updates the db.
+    async selectSimilar(photo, iSimilar, similarId) {
+      const updateArrayInItem = async (item, index) => {
+        let arr = item.neighbors
+        arr[index].isSamePhoto = true
+
+        let payload = {
+          ...item,
+          arr
+        }
+
+        const payloadId = item._id
+        const updatedItem = await axios.post(
+          `http://localhost:3000/api/${payloadId}`,
+          payload
+        )
+        return updatedItem
+      }
+
+      const res = await axios.get(`http://localhost:3000/api/${similarId}`)
+      const similarPhoto = res.data[0]
+
+      const indexPhoto = _.findIndex(similarPhoto.neighbors, {
+        filename: photo._id
+      })
+
+      updateArrayInItem(photo, iSimilar)
+      updateArrayInItem(similarPhoto, indexPhoto)
+
+      console.log('procurando da foto:')
+      console.log(similarPhoto)
+      console.log('o index da foto mãe:', photo._id, 'que é:', indexPhoto)
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.similars {
+  display: flex;
+}
+
+.photo-card {
+  display: flex;
+  flex-direction: row;
+  margin: 40px;
+  color: #eaeaea;
+}
+.photo-item {
+  margin: 20px;
+  cursor: pointer;
+}
+.photo {
+  height: 300px;
+}
+
+.active {
+  outline: 3px solid #49bd79;
+}
+</style>
---
+++ server/index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+app.use(express.json())
+
+const datastore = require('nedb')
+const db = new datastore('./database/database.db')
+
+db.loadDatabase()
+
+app.get('/api', (req, res) => {
+  if (req.query) {
+    const { similarity } = req.query
+    db.find({ "neighbors": { $elemMatch: { similarity: { $gt: parseFloat(similarity) } } } }, (err, data) => {
+      res.json(data)
+    })
+  } else {
+    db.find({}, (err, data) => {
+      res.json(data)
+    })
+  }
+
+})
+
+app.get('/api/:id', (req, res) => {
+  const { id } = req.params
+  db.find({ _id: id }, (err, data) => {
+    res.json(data)
+  })
+})
+
+app.post('/api/:id', (req, res) => {
+  const { id } = req.params
+  const payload = req.body
+
+  db.update({ _id: id }, payload, { returnUpdatedDocs: true }, (err, numAffected, affectedDocuments, upsert) => {
+    res.json(affectedDocuments)
+    console.log(affectedDocuments)
+  })
+
+})
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const { host, port } = nuxt.options.server
+
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
+  }
+
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
